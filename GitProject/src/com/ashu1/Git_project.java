7dc7588a28a8d92174b6d642cbae6e265c8b0b57
akash <akhouriakash10@gmail.com>
2024-09-16 22:57:19 +0530

    this code is commit

---
+++ GitProject/src/com/ashu1/Git_project.java
@@ -0,0 +1,10 @@
+package com.ashu1;
+
+public class Git_project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey..!, ashu your project started.");
+	}
+
+}
